ba6128bcfe27613c31986ebe3aa45d4671330d28
Kresza1337 <krechaxdkrechaxd@gmail.com>
2024-04-18 12:03:09 +0200

    thymeleaf - flight list - add new flight operations

--- src/main/java/com/jkryska/LOTsystem/restcontroller/FlightIndexController.java
+++ src/main/java/com/jkryska/LOTsystem/restcontroller/FlightIndexController.java
@@ -5,11 +5,10 @@ import com.jkryska.LOTsystem.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class FlightIndexController {
@@ -27,12 +26,25 @@ public class FlightIndexController {
     public String createFlight(Model model){
         Flight flight = new Flight();
         model.addAttribute("flight", flight);
-        return "create_flight"; // Zwrócenie nazwy widoku
+        return "create_flight";
     }
 
     @PostMapping("/create_flight")
     public String saveFlight(@ModelAttribute("flight") Flight flight){
         flightRepository.save(flight);
+        return "redirect:/flights";
+    }
+
+    @GetMapping("/delete_flight")
+    public String showDeleteFlight(Model model){
+        List<Flight> flights = flightRepository.findAll();
+        model.addAttribute("flights", flights);
+        return "/delete_flight";
+    }
+    @PostMapping("/delete_flight")
+    String deleteFlight(@RequestParam("id") Long id){
+        Optional<Flight> flight = flightRepository.findById(id);
+        flightRepository.deleteById(id);
         return "redirect:/flights";
     }
 }
